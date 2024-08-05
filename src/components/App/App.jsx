e691fe085a67017b8ead5b44a7a39f04634b0395
champix56 <desorbaix@free.fr>
2024-08-05 14:40:20 +0200

    passage full jsx

---
+++ src/components/App/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Button from "../ui/Button/Button.jsx";
+function App() {
+  return (
+    <div className="App">DEMAT BREIZH</div>
+  );
+}
+
+export default App;
--- src/components/App/App.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function App() {
-  return <div> DEMAT BREIZH</div>
-}
-
-export default App
