9cc0a9064216a30d00a5031dceffb35a2537772a
champix56 <desorbaix@free.fr>
2024-08-05 17:12:01 +0200

    reformat

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,9 +5,22 @@ import ButtonStyle from "../ui/Button/Button.module.css";
 function App() {
   return (
     <div className="App">
-      <Button bgcolor="skyblue" color="black">Valider</Button>
-      <Button bgcolor="skyblue" color="white" >Annuler</Button>
-      <Button style={{textDecoration:'underline', fontWeight:500, fontSize:'25pt'}} ><img src="/cross.png"/>Cancel</Button>
+      <Button bgcolor="skyblue" color="black">
+        Valider
+      </Button>
+      <Button bgcolor="skyblue" color="white">
+        Annuler
+      </Button>
+      <Button
+        style={{
+          textDecoration: "underline",
+          fontWeight: 500,
+          fontSize: "25pt",
+        }}
+      >
+        <img src="/cross.png" />
+        Cancel
+      </Button>
     </div>
   );
 }
