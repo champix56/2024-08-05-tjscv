beffb1a2b7894a635d83264d168ca221d6d35a93
champix56 <desorbaix@free.fr>
2024-08-05 16:35:28 +0200

    passage par props des styles distincts

--- src/components/ui/Button/Button.jsx
+++ src/components/ui/Button/Button.jsx
@@ -4,7 +4,11 @@ import style from "./Button.module.css";
 const Button = (props) => {
   console.log(props);
   return (
-    <div className={style.Button+' '+style[props.buttonStyle]} data-testid="Button">
+    <div
+      style={{ backgroundColor: props.bgcolor, color: props.color }}
+      className={style.Button}
+      data-testid="Button"
+    >
       {props.children}
     </div>
   );
