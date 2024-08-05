13fbdb68739625e7b8c8157982ca6c7fa30fd399
champix56 <desorbaix@free.fr>
2024-08-05 17:20:43 +0200

    defaultProps

--- src/components/ui/Button/Button.jsx
+++ src/components/ui/Button/Button.jsx
@@ -18,11 +18,14 @@ const Button = (props) => {
   );
 };
 Button.propTypes = {
-  bgcolor: PropTypes.string.isRequired,
+  bgcolor: PropTypes.string,
   color: PropTypes.oneOf(["white", "black"]).isRequired,
   style: PropTypes.exact({
     textDecoration: PropTypes.oneOf(["underline", "none"]),
     fontWeight: PropTypes.oneOf([100, 500, 900]),
   }),
+};
+Button.defaultProps = {
+  bgcolor: "yellow",
 };
 export default Button;
