a783a68fd34373cc19c58607a1ca0d144c495c95
champix56 <desorbaix@free.fr>
2024-08-05 17:16:53 +0200

    reformat

--- src/components/ui/Button/Button.jsx
+++ src/components/ui/Button/Button.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import style from "./Button.module.css";
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 const Button = (props) => {
   console.log(props);
   return (
     <div
-      style={{...props.style, backgroundColor: props.bgcolor, color: props.color }}
+      style={{
+        ...props.style,
+        backgroundColor: props.bgcolor,
+        color: props.color,
+      }}
       className={style.Button}
       data-testid="Button"
     >
@@ -13,12 +17,12 @@ const Button = (props) => {
     </div>
   );
 };
-Button.propTypes={
-    bgcolor: PropTypes.string.isRequired,
-    color: PropTypes.oneOf(['white','black']).isRequired,
-    style: PropTypes.exact({
-      textDecoration: PropTypes.oneOf(['underline','none']),
-      fontWeight: PropTypes.oneOf([100,500,900])
-    }),
-}
+Button.propTypes = {
+  bgcolor: PropTypes.string.isRequired,
+  color: PropTypes.oneOf(["white", "black"]).isRequired,
+  style: PropTypes.exact({
+    textDecoration: PropTypes.oneOf(["underline", "none"]),
+    fontWeight: PropTypes.oneOf([100, 500, 900]),
+  }),
+};
 export default Button;
