d5f4fa582004e5dd39f6789d4987b157f535c5ff
champix56 <desorbaix@free.fr>
2024-08-06 17:18:41 +0200

    fetch de data sur composant

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import FlexWFirstGrow from "../layout/FlexWFirstGrow/FlexWFirstGrow";
 import FlexHThirdGrow from "../layout/FlexHThirdGrow/FlexHThirdGrow";
 import Header from "../ui/Header/Header";
@@ -9,7 +9,15 @@ import Footer from "../ui/Footer/Footer";
 import { MemeSVGViewer, emptyMeme } from "orsys-tjs-meme";
 
 const App = (props) => {
-  const [meme, setMeme] = useState(emptyMeme)
+  const [meme, setMeme] = useState(emptyMeme);
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5679/images")
+      .then((r) => r.json())
+      .then((arr) => setImages(arr));
+  }, []);
+
   return (
     <div className="App">
       <FlexHThirdGrow>
@@ -19,6 +27,7 @@ const App = (props) => {
           <MemeSVGViewer image={undefined} meme={meme} basePath="" />
           <MemeForm />
         </FlexWFirstGrow>
+        <div>{JSON.stringify(images)}</div>
         <Footer />
       </FlexHThirdGrow>
     </div>
