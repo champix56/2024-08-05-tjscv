6b81eb0b6d0620500c9776ab242aef56304d8e80
champix56 <desorbaix@free.fr>
2024-08-06 15:01:12 +0200

    base de construction pour app

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import FlexHThirdGrow from "../layout/FlexHThirdGrow/FlexHThirdGrow";
+import Header from "../ui/Header/Header";
+import NavBar from "../ui/NavBar/NavBar";
+import MemeSVGViewer from "../ui/MemeSVGViewer/MemeSVGViewer";
+import MemeForm from "../functionnal/MemeForm/MemeForm";
+import Footer from "../ui/Footer/Footer";
 
-const App=(props)=> {
+const App = (props) => {
   return (
     <div className="App">
-    
+      <FlexHThirdGrow>
+        <Header />
+        <NavBar />
+        <FlexWFirstGrow>
+          <MemeSVGViewer />
+          <MemeForm />
+        </FlexWFirstGrow>
+        <Footer />
+      </FlexHThirdGrow>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
