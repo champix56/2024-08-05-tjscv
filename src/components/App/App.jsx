f90d0c1baa04e8e524f7f96248a7461a44d56db4
champix56 <desorbaix@free.fr>
2024-08-06 15:11:00 +0200

    base fonctionnel du projet

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import FlexWFirstGrow from "../layout/FlexWFirstGrow/FlexWFirstGrow";
 import FlexHThirdGrow from "../layout/FlexHThirdGrow/FlexHThirdGrow";
 import Header from "../ui/Header/Header";
 import NavBar from "../ui/NavBar/NavBar";
-import MemeSVGViewer from "../ui/MemeSVGViewer/MemeSVGViewer";
+import MemeSVGViewer from '../ui/MemeSVGViewer/MemeSVGViewer'
 import MemeForm from "../functionnal/MemeForm/MemeForm";
 import Footer from "../ui/Footer/Footer";
 
