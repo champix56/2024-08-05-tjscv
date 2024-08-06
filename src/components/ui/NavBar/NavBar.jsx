79f93a6dd6050d015e47d6fd28cbb851559336a5
champix56 <desorbaix@free.fr>
2024-08-06 15:50:37 +0200

    navbar react-bootstrap 5

--- src/components/ui/NavBar/NavBar.jsx
+++ src/components/ui/NavBar/NavBar.jsx
@@ -1,17 +1,20 @@
 import React from "react";
-import PropTypes from "prop-types";
-import styles from "./NavBar.module.css";
-
-const NavBar = (props) => {
+import { Container, Nav, Navbar } from "react-bootstrap";
+import '../../../../node_modules/bootstrap/dist/css/bootstrap.css'
+import '../../../../node_modules/bootstrap/dist/js/bootstrap.js'
+const NavBar = () => {
   return (
-    <div className={styles.NavBar} data-testid="NavBar">
-      NavBar Component
-    </div>
+    <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
   );
 };
-
-NavBar.propTypes = {};
-
-NavBar.defaultProps = {};
 
 export default NavBar;
