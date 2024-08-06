64178508aff99ab8fef034f9cde6611c51d44f76
champix56 <desorbaix@free.fr>
2024-08-06 12:36:24 +0200

    templates generique

---
+++ grc_templates/hookedFunction/TemplateName.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import styles from "./TemplateName.module.css";
+export const templateNameInitialState={};
+const TemplateName = (props) => {
+const [state, setstate] = useState(templateNameInitialState)
+useEffect(() => {
+  //mount & update de state
+}, [state])
+useEffect(() => {
+  //mount
+  return () => {
+    //unmount
+  };
+}, [])
+  return (
+    <div className={styles.TemplateName} data-testid="TemplateName">
+      TemplateName Component<br/>{JSON.stringify(state)}
+    </div>
+  );
+};
+
+TemplateName.propTypes = {};
+
+TemplateName.defaultProps = {};
+
+export default TemplateName;
