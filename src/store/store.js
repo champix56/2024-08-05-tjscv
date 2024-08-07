dc375b8514f8ba415c602921e3c9c6de76fe6365
champix56 <desorbaix@free.fr>
2024-08-07 11:56:03 +0200

    clean store

--- src/store/store.js
+++ src/store/store.js
@@ -1,16 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { emptyMeme } from "orsys-tjs-meme";
 import ressourcesReducer, { addImage, addImages } from "./ressources";
 import { images } from "../../db/db.json";
-// const initialCurrent = emptyMeme;
-/*const reducerCurrent = (state = initialCurrent, action) => {
-  switch (action.type) {
-    case "CURRENT/CHANGE_CURENT":
-      return { ...state, ...action.payload };
-    default:
-      return state;
-  }
-};*/
+
 export const store = configureStore({ reducer: ressourcesReducer });
 store.subscribe(() => {
   console.log(store.getState());
