0eb5a6b9b3c1e8419e82013a2b96bc2539414fdb
champix56 <desorbaix@free.fr>
2024-08-07 16:46:12 +0200

    ajout du contexte de routage

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import App from "./components/App/App";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
   </React.StrictMode>
 );
