7615256e3737af271fd0bef419d171a4d3ca8bf9
champix56 <desorbaix@free.fr>
2024-08-07 12:57:34 +0200

    connexion a la tranche current

--- src/components/functionnal/MemeForm/MemeForm.connected.jsx
+++ src/components/functionnal/MemeForm/MemeForm.connected.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import UNCONNECTEDMemeForm from "./MemeForm";
 import { useDispatch, useSelector } from "react-redux";
+import { changeMeme } from "../../../store/current";
 const MemeForm = (props) => {
   const images = useSelector((storeState) => {
     return storeState.ressources.images;
   });
-//   const dispatch = useDispatch();
+  const meme = useSelector((storeState) => {
+    return storeState.current;
+  });
+  const dispatch = useDispatch();
   return (
     <UNCONNECTEDMemeForm
       {...props}
       images={images}
-    //   onMemeChange={(meme) => dispatch(changeMeme(meme))}
+      meme={meme}
+      onMemeChange={(meme) => dispatch(changeMeme(meme))}
     />
   );
 };
