eb14a89b2cdfd5a7b7addfb6e6c8be288390c315
champix56 <desorbaix@free.fr>
2024-08-07 17:06:12 +0200

    thumbnail unconnected

--- src/pages/MemeThumbnail/MemeThumbnail.jsx
+++ src/pages/MemeThumbnail/MemeThumbnail.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
-import {MemeSVGViewer} from 'orsys-tjs-meme'
-import style from './MemeThumbnail.module.css'
+import React from "react";
+import { MemeSVGViewer } from "orsys-tjs-meme";
+import style from "./MemeThumbnail.module.css";
+import PropTypes from "prop-types";
 export default function MemeThumbnail(props) {
   return (
-    <div className={style.MemeThumbnail}>MemeThumbnail</div>
-  )
+    <div className={style.MemeThumbnail}>
+      {props.memes.map((m, i) => {
+        return (
+          <div key={"t" + i}>
+            <MemeSVGViewer meme={m} image={props.images.find((image) => 
+          image.id === m.imageId
+        )} basePath="" />
+            <br />
+            {m.titre}
+          </div>
+        );
+      })}
+    </div>
+  );
 }
+MemeThumbnail.propTypes = {
+  images: PropTypes.array.isRequired,
+  memes: PropTypes.array.isRequired,
+};
