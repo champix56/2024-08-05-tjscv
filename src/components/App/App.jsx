1e543442e8bb15b525a040f9446116b87fb9d995
champix56 <desorbaix@free.fr>
2024-08-07 14:50:20 +0200

    clean unused add default memesvg connected

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,20 +6,9 @@ import NavBar from "../ui/NavBar/NavBar";
 
 import MemeForm from "../functionnal/MemeForm/MemeForm.connected";
 import Footer from "../ui/Footer/Footer";
-import { emptyMeme } from "orsys-tjs-meme";
 
-import {store} from '../../store/store'
 import MemeSVGViewer from "../ui/MemeSVGViewer/MemeSVGViewer";
 const App = (props) => {
-  const [meme, setMeme] = useState({...emptyMeme, text:"React c'est super"});
-  const [images, setImages] = useState([]);
-
-  useEffect(() => {
-    fetch("http://localhost:5679/images")
-      .then((r) => r.json())
-      .then((arr) => setImages(arr));
-  }, []);
-
   return (
     <div className="App">
       <FlexHThirdGrow>
@@ -29,7 +18,6 @@ const App = (props) => {
           <MemeSVGViewer basePath="" />
           <MemeForm />
         </FlexWFirstGrow>
-        <div>{JSON.stringify(images).substring(1,10)}</div>
         <Footer />
       </FlexHThirdGrow>
     </div>
