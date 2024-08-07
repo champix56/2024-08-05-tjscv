9928a1bc1af8b3230d8d8c9cab76630af892f879
champix56 <desorbaix@free.fr>
2024-08-07 12:05:26 +0200

    ajout de combine meme pour une seul tranche

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,11 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import ressourcesReducer, { addImage, addImages } from "./ressources";
 import { images } from "../../db/db.json";
+import { reducerCurrent } from "./current";
 
-export const store = configureStore({ reducer: ressourcesReducer });
+export const store = configureStore({
+  reducer: combineReducers({ ressources: ressourcesReducer }),
+});
 store.subscribe(() => {
   console.log(store.getState());
 });
