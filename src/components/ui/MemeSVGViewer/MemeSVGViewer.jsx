62b7063e3045015f680b04ca64071f52b85fc80e
champix56 <desorbaix@free.fr>
2024-08-07 14:50:37 +0200

    add default props pour memesvg connected

--- src/components/ui/MemeSVGViewer/MemeSVGViewer.jsx
+++ src/components/ui/MemeSVGViewer/MemeSVGViewer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { MemeSVGViewer as UNCONNECTEDMemeSVGViewer } from "orsys-tjs-meme";
-import { useSelector } from "react-redux";
+import { useSelector, useStore } from "react-redux";
 import PropTypes from "prop-types";
 
 const MemeSVGViewer = (props) => {
@@ -13,6 +13,7 @@ const MemeSVGViewer = (props) => {
   return <UNCONNECTEDMemeSVGViewer {...props} meme={meme} image={image} />;
 };
 MemeSVGViewer.propTypes = {
-  basePath: PropTypes.oneOf(["", "/", "/img"]),
+  basePath: PropTypes.oneOf(["", "/", "/img"]).isRequired,
 };
+MemeSVGViewer.defaultProps = { basePath: "" };
 export default MemeSVGViewer;
