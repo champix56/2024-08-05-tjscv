8756dc8cbe73dd527e9fbbdba8c51efa00cb6349
champix56 <desorbaix@free.fr>
2024-08-07 14:42:45 +0200

    reformat

--- src/components/ui/MemeSVGViewer/MemeSVGViewer.jsx
+++ src/components/ui/MemeSVGViewer/MemeSVGViewer.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { MemeSVGViewer as UNCONNECTEDMemeSVGViewer } from "orsys-tjs-meme";
 import { useSelector } from "react-redux";
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 
 const MemeSVGViewer = (props) => {
   const meme = useSelector((s) => s.current);
-  const image=useSelector((s)=>s.ressources.images.find(i=>i.id===s.current.imageId));
+  const image = useSelector((s) =>
+    s.ressources.images.find((i) => i.id === s.current.imageId)
+  );
   /*const images = useSelector((s) => s.ressources.images);
   const image = images.find((i) => i.id === meme.imageId);*/
   return <UNCONNECTEDMemeSVGViewer {...props} meme={meme} image={image} />;
 };
-MemeSVGViewer.propTypes={
-  basePath: PropTypes.oneOf(['','/','/img'])
-}
+MemeSVGViewer.propTypes = {
+  basePath: PropTypes.oneOf(["", "/", "/img"]),
+};
 export default MemeSVGViewer;
